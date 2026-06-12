7d787b825b29b433899b0fa86be99a7c80d0b56a
Devendradhote001 <ddhote780@gmail.com>
2026-06-12 18:45:51 +0530

    first push

---
+++ start/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.send("okk me tumhara server hu");
+});  
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
